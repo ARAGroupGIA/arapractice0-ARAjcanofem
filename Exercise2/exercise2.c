ce284d3f2b3ce9cb56813f86931a2d053ff2e717
ARAjcanofem <55189151+ARAjcanofem@users.noreply.github.com>
2019-09-11 14:29:26 +0200

    version 1

--- Exercise2/exercise2.c
+++ Exercise2/exercise2.c
@@ -1,5 +1,15 @@
 /* Here, you should entry the required libraries */
 
+#include <stdio.h>
+
 void main(){
 	/* Here, you should write the source code of your program */
+	int A,B,C;
+	printf("entry value") ;
+	scanf("%d",&A);
+	printf("entry value") ;
+	scanf("%d",&B);
+    C=A+B;
+    printf("show %d",C);
+ 	
 }
