420d71ec542eb56bcf53ca030046611d42e3d62b
ARAjcanofem <55189151+ARAjcanofem@users.noreply.github.com>
2019-09-11 14:01:31 +0200

    version 1

--- Exercise1/exercise1.c
+++ Exercise1/exercise1.c
@@ -2,8 +2,8 @@
 /* Implement a program that it shows the message "Hello World" on screen. */
 /*************************************************************************/
 
-/* Here, you must include the required libraries */
+/* Here, you must include the required libraries */#include<stdio.h>
 
 void main(){
-	/* Here, you must write the source code to show the missatge "Hello world" on screen*/
+	/* Here, you must write the source code to show the missatge "Hello world" on screen*/printf("Hello World vertion 1");
 }
